6161a5d52afb091ff8573a86848fc916767dc439
slscott9 <baseballdude9cf@gmail.com>
2020-10-28 17:42:05 -0500

    initial commit

--- models/folder.models.js
+++ models/folder.models.js
@@ -1,7 +1,9 @@
 /*
-    Define folder objects here 
 
-    Write Folder.extention functions that will be database query methods
+    This is a repository but the models are in here as well
+
+
+    Write Folder.extention functions that will be database query methods 
 
     end with module.exports = Customer This will allow the whole application to use these extention function when using const folder = require("/.folder.js")
     In another file
@@ -11,3 +13,7 @@
 
     Use sqlDatabase.query("sql statement") inside of Folder.extention functions to open connection and insert or query database
 */
+
+const sqlDatabase = require("./database.js")
+
+//constructor for Folder object
